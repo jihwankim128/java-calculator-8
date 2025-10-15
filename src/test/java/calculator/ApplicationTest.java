5f2e3aa79283c52bf43dc8e5592b5cef8a6da45c
jihwankim128 <jihwan1535@nate.com>
2025-10-15 09:00:47 +0900

    test: 엣지 케이스 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ApplicationTest extends NsTest {
     
@@ -33,12 +35,30 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    @Test
-    void 예외_테스트() {
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "-1,2,3", " ", "1,2,", "1, 2, 3", "//.-\\n1", "//.\\n//-\\n1", "a//.-\\n1"})
+    void 예외_테스트(String invalidExpression) {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
+            assertThatThrownBy(() -> runException(invalidExpression))
                 .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 큰_수_입력_테스트() {
+        // given : 큰 수가 주어진다면,
+        String maxLongValue = String.valueOf(Long.MAX_VALUE);
+        int maxLongLength = maxLongValue.length();
+        String firstBigNumber = "1".repeat(maxLongLength + 1);
+        String secondBigNumber = "2".repeat(maxLongLength + 1);
+        String bigNumberExpression = firstBigNumber + "," + secondBigNumber;
+
+        // when & then : 큰수에 대한 연산 결과가 출력된다.
+        String bigNumberSumResult = "3".repeat(maxLongLength + 1);
+        assertSimpleTest(() -> {
+            run(bigNumberExpression);
+            assertThat(output()).contains("결과 : " + bigNumberSumResult);
+        });
     }
 
     @Override
