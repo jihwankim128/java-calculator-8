eba1f9753772c7a9ddd542bf3b9f8282397c494d
jihwankim128 <jihwan1535@nate.com>
2025-10-18 19:32:51 +0900

    test: Positive Number 테스트 추가

---
+++ src/test/java/calculator/domain/PositiveNumberTest.java
@@ -0,0 +1,35 @@
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigInteger;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class PositiveNumberTest {
+
+    @Test
+    void 양수가_주어지면_PositiveNumber를_생성할_수_있다() {
+        // given
+        BigInteger positiveNumber = BigInteger.valueOf(1);
+
+        // when
+        PositiveNumber result = PositiveNumber.of(positiveNumber);
+
+        // then
+        assertThat(result.getValue()).isEqualTo(positiveNumber);
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {0, -1})
+    void 양수가_아니라면_PositiveNumber_생성시_예외가_발생한다(long value) {
+        // given
+        BigInteger negativeNumber = BigInteger.valueOf(value);
+
+        // when & then
+        assertThatThrownBy(() -> PositiveNumber.of(negativeNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
