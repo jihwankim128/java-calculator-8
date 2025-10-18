808237187f20461cf542c44b3f237231e4c6a5c2
jihwankim128 <jihwan1535@nate.com>
2025-10-18 19:14:35 +0900

    test: 표현식 처리 테스트 추가

--- src/main/java/calculator/domain/DelimiterTokenizer.java
+++ src/main/java/calculator/domain/DelimiterTokenizer.java
@@ -2,7 +2,7 @@ package calculator.domain;
 
 public abstract class DelimiterTokenizer {
 
-    public String[] tokenize(String expression) {
+    public final String[] tokenize(String expression) {
         if (expression == null || expression.isEmpty()) {
             return new String[0];
         }
---
+++ src/test/java/calculator/domain/toknizer/SimpleDelimiterTokenizerTest.java
@@ -0,0 +1,60 @@
+package calculator.domain.toknizer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class SimpleDelimiterTokenizerTest {
+
+    SimpleDelimiterTokenizer tokenizer = new SimpleDelimiterTokenizer();
+
+    @Test
+    void 기본_구분자가_포함된_표현식이_주어지면_구분자로_분리한다() {
+        // given
+        String expression = "1,2:3";
+
+        // when
+        String[] result = tokenizer.tokenize(expression);
+
+        // then
+        assertThat(result).contains("1", "2", "3");
+    }
+
+    @Test
+    void 구분자가_마지막에_포함된_경우_마지막에_빈_값을_포함하여_분리한다() {
+        // given
+        String expression = "1,2,";
+
+        // when
+        String[] result = tokenizer.tokenize(expression);
+
+        // then
+        assertThat(result).contains("1", "2", "");
+    }
+
+    @Test
+    void 커스텀_구분자가_포함된_표현식이_주어지면_기본_구분자를_포함해_구분한다() {
+        // given
+        String expressionWithCustomDelimiter = "//.\\n1.2";
+        
+        // when
+        String[] result = tokenizer.tokenize(expressionWithCustomDelimiter);
+
+        // then
+        assertThat(result).contains("1", "2");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "//-.\\n1",         // 1. Custom 구분자로 '문자'가 아닌 '문자열'이 입력된 경우
+            "//-\\n//.\\n1"     // 2. Custom 구분자 입력이 2개 이상인 경우
+    })
+    void 잘못된_커스텀_구분자_입력시_예외가_발생한다(String invalidExpression) {
+        // when & then
+        assertThatThrownBy(() -> tokenizer.tokenize(invalidExpression))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
